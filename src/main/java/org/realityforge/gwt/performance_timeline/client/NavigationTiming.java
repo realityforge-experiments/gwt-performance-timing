be2f44ad9fbbdb6c9371b6fe59e4408bf6b80d19
Peter Donald <peter@realityforge.org>
2013-12-23 09:18:29 +1100

    Add some basic javadocs

--- src/main/java/org/realityforge/gwt/performance_timeline/client/NavigationTiming.java
+++ src/main/java/org/realityforge/gwt/performance_timeline/client/NavigationTiming.java
@@ -1,5 +1,12 @@
 package org.realityforge.gwt.performance_timeline.client;
 
+/**
+ * The interface defined by the first public draft of the Navigation Timing L2 specification.
+ *
+ * See <a href="http://www.w3.org/TR/navigation-timing-2/">Navigation Timing L2</a> for further details.
+ *
+ * This is currently supported in IE11.
+ */
 public interface NavigationTiming
   extends PerformanceEntry
 {
